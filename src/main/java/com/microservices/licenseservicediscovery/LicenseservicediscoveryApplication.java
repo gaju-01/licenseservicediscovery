99b9c05ca93fd845ae95a953925b7741fd2d7b75
gaju-01 <you@example.com>
2024-10-03 22:06:40 +0530

    First commit

---
+++ src/main/java/com/microservices/licenseservicediscovery/LicenseservicediscoveryApplication.java
@@ -0,0 +1,13 @@
+package com.microservices.licenseservicediscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LicenseservicediscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LicenseservicediscoveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/microservices/licenseservicediscovery/LicenseservicediscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.licenseservicediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LicenseservicediscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
